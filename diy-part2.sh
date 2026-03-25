879b2fb9117feb50f1b172185caf3848757fdcf9
newsqualychen <squaly.chen@gmail.com>
2026-03-25 08:06:29 +0800

    Update default IP address in diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,7 +19,7 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 #!/bin/bash
-# 1. 修改默认 IP 为 10.10.10.252
+# 1. 修改默认 IP 为 10.10.10.212
 sed -i 's/192.168.1.1/10.10.10.252/g' package/base-files/files/bin/config_generate
 
 # 2. 修改默认主机名
